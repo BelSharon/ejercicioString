b44e6bc5cbe89d600b2b63c1b4656d41ce3733ee
SBTR <03sh.bel.tr@gmail.com>
2022-12-13 10:44:58 -0600

    Ejercicio atrasado de validación

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function reves(str) {
     return nuevaStr;
 }
 
-//Ejercicio 3 Nos ayuda a saber si una palabra es un palíndromo.
+//Ejercicio 3------------------------------------------------
 function esPalindromo(str) {
     str = str.toLowerCase();
     str = str.replaceAll(" ","");
@@ -37,3 +37,35 @@ function esPalindromo(str) {
         return true;
     return false;
 }
+
+
+//VALIDACIONES------------------------------------------------
+let btnEnviar = document.getElementById("btnEnviar");
+    
+    btnEnviar.addEventListener("click", function (event) {
+    event.preventDefault();
+    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
+    let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
+    let alertError = document.getElementById("alertError");
+    exampleFormControlTextarea1.value = exampleFormControlTextarea1.value.trim();
+    alertError.style.display="none";
+    alertError.innerHTML ="";
+    // console.log("[" +
+    //     exampleFormControlTextarea1.value.replaceAll("  ", "") + "]");
+
+    //     console.log(exampleFormControlTextarea1.value.replaceAll("  ","").length);
+
+    if (exampleFormControlTextarea1.value.trim().replaceAll("  ","").length < 20) {
+        alertError.innerHTML = `El mensaje debe contener 20 caracteres o más`;
+        alertError.style.display="block";
+        exampleFormControlTextarea1.focus();
+        exampleFormControlTextarea1.select();
+    }
+
+   if ( exampleFormControlInput1.value.match(emailRegex) == null) {
+        alertError.style.display="block";
+        alertError.innerHTML+= "<br/> El correo electrónico no es válido.";
+   }
+
+})
